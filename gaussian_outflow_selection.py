9b1dffa15b8754c9faaff900339eab638a6ca737
Ivan Kostyuk <kostyuk@hotmail.de>
2024-04-17 18:50:13 +0200

    Enabled option to provide number of peaks. Used internal functionality to group particles.

--- gaussian_outflow_selection.py
+++ gaussian_outflow_selection.py
@@ -5,29 +5,56 @@ from scipy.stats import norm
 from sklearn.mixture import GaussianMixture as GMM
 
 
-def select_number_of_peaks(gas):
-    x = gas["Flow_Velocities"]
-    x = x.reshape(-1, 1)
-
+def get_opt_bic(data, min_number, max_number):
     bics = []
+    counters = []
     min_bic = 0
     counter = 1
-    num_bics = 5
-    for _ in range(num_bics):
+    for _ in range(min_number, max_number + 1):
         gmm = GMM(
             n_components=counter,
             max_iter=5000,
-            random_state=0,
+            random_state=42,
             covariance_type="full",
         )
-        _ = gmm.fit(x).predict(x)
-        bic = gmm.bic(x)
+        _ = gmm.fit(data).predict(data)
+        bic = gmm.bic(data)
         bics.append(bic)
         if bic < min_bic or min_bic == 0:
             min_bic = bic
             opt_bic = counter
         counter = counter + 1
-    return opt_bic, bics
+        counters.append(counter)
+    return opt_bic, bics, counters
+
+
+def get_data(gas, keys):
+    if len(keys) == 1:
+        data = gas[keys[0]]
+        data = data.reshape(-1, 1)
+    else:
+        data = []
+        for key in keys:
+            if gas[key].ndim == 1:
+                data.append(gas[key])
+            elif gas[key].ndim == 2:
+                for i in range(gas[key].shape[1]):
+                    data.append(gas[key][:, i])
+            else:
+                raise NotImplementedError(
+                    f"Can't handle data with dim {gas[key].ndim}"
+                )
+        data = np.array(data).T
+    return data
+
+
+def select_number_of_peaks(gas, keys, min_number, max_number):
+    data = get_data(gas=gas, keys=keys)
+
+    opt_bic, bics, counter = get_opt_bic(
+        data=data, min_number=min_number, max_number=max_number
+    )
+    return opt_bic, bics, counter
 
 
 def get_probs(v_out, pdfs, weights):
@@ -36,28 +63,40 @@ def get_probs(v_out, pdfs, weights):
     )
 
 
-def associate_gas_to_peaks(gas, n_peaks):
+def associate_gas_to_peaks(gas, n_peaks, props):
     gmm = GMM(
         n_components=n_peaks,
         max_iter=5000,
-        random_state=10,
         covariance_type="full",
+        random_state=42,
     )
-    x = gas["Flow_Velocities"]
-    x = x.reshape(-1, 1)
-    means = gmm.fit(x).means_
-    stds = np.sqrt(gmm.fit(x).covariances_)
-    weights = gmm.fit(x).weights_
-    pdfs = []
-    for i in range(n_peaks):
-        pdfs.append(stats.norm(loc=means[i][0], scale=stds[i][0][0]))
-    probs = get_probs(gas["Flow_Velocities"], pdfs, weights)
-    gas["group"] = np.argmax(probs, axis=0) + 1
+    data = get_data(gas, keys=props)
+    gmm.fit(data)
+    # means = gmm.fit(x).means_
+    # stds = np.sqrt(gmm.fit(x).covariances_)
+    # weights = gmm.fit(x).weights_
+    # pdfs = []
+    # for i in range(n_peaks):
+    #     pdfs.append(stats.norm(loc=means[i][0], scale=stds[i][0][0]))
+    # probs = get_probs(gas["Flow_Velocities"], pdfs, weights)
+    # gas["group"] = np.argmax(probs, axis=0) + 1
+    gas["group"] = gmm.predict(data)
     return
 
 
-def group_gas(gas):
-    opt_bic, bics = select_number_of_peaks(gas)
-    print(f"Selecting {opt_bic} peaks. The bic values obtained were {bics}")
-    associate_gas_to_peaks(gas, n_peaks=opt_bic)
-    return opt_bic
+def group_gas(
+    gas,
+    props=["Flow_Velocities"],
+    min_number=1,
+    max_number=10,
+    peak_number=None,
+):
+    if peak_number is None:
+        n_peak, bics, counters = select_number_of_peaks(
+            gas, keys=props, min_number=min_number, max_number=max_number
+        )
+        print(f"Selecting {n_peak} peaks. The bic values obtained were {bics}")
+    else:
+        n_peak = peak_number
+    associate_gas_to_peaks(gas, n_peaks=n_peak, props=props)
+    return n_peak
