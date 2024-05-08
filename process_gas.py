5bd312fd59a2ae3b4b92002495cbc7e723a68c85
Ivan Kostyuk <kostyuk@hotmail.de>
2024-05-08 18:22:12 +0200

    Implemented lazy initialization for resource intensive properties

--- process_gas.py
+++ process_gas.py
@@ -21,134 +21,161 @@ from utils import (
 
 
 class Galaxy:
+
+    _, sim_path = get_sim()
+
     def __init__(
         self,
         df,
         halo_id,
         snap,
         with_vesc=False,
-        cut_factor=50,
+        cut_factor=10,
         n_peak=4,
         group_props=None,
     ):
+
+        self._halo = None
+        self._gal_pos = None
+        self._gal_vel = None
+        self._stars = None
+        self._gas = None
+
         self.df = df
         self.halo_id = halo_id
         self.snap = snap
-        self.halo = self.get_halo()
         self.galaxy_id = int(self.halo.idx)
         self.with_vesc = with_vesc
-        _, self.sim_path = get_sim()
-        self.get_galaxy_pos()
-        self.get_galaxy_vel()
-        self.scale_radius = float(self.halo.Galaxy_SHMR)
+        self.scale_radius = float(self.halo.r_SFR)
         self.cut_factor = cut_factor
         self.r_vir = float(self.halo.R_vir)
         self.n_peak = n_peak
         self.group_props = group_props
 
-    def get_halo(self):
-        halo = self.df[
-            (self.df.Halo_id == self.halo_id) & (self.df.snap == self.snap)
-        ]
-        return halo
-
-    def get_galaxy_pos(self):
-        self.gal_pos = np.array(
-            [
-                float(self.halo.Galaxy_pos_x),
-                float(self.halo.Galaxy_pos_y),
-                float(self.halo.Galaxy_pos_z),
+    @property
+    def halo(self):
+        if self._halo is None:
+            self._halo = self.df[
+                (self.df.Halo_id == self.halo_id) & (self.df.snap == self.snap)
             ]
-        )
-        return
+        return self._halo
 
-    def get_galaxy_vel(self):
-        self.galaxy_vel = np.array(
-            [
-                float(self.halo.Galaxy_vel_x),
-                float(self.halo.Galaxy_vel_y),
-                float(self.halo.Galaxy_vel_z),
-            ]
-        )
-        return
+    @property
+    def gal_pos(self):
+        if self._gal_pos is None:
+            self._gal_pos = np.array(
+                [
+                    float(self.halo.Galaxy_pos_x),
+                    float(self.halo.Galaxy_pos_y),
+                    float(self.halo.Galaxy_pos_z),
+                ]
+            )
+        return self._gal_pos
 
-    def get_relative_coordinates(self, particles):
+    @property
+    def gal_vel(self):
+        if self._gal_vel is None:
+            self._gal_vel = np.array(
+                [
+                    float(self.halo.Galaxy_vel_x),
+                    float(self.halo.Galaxy_vel_y),
+                    float(self.halo.Galaxy_vel_z),
+                ]
+            )
+        return self._gal_vel
+
+    @property
+    def stars(self):
+        if self._stars is None:
+            self._stars = il.snapshot.loadSubhalo(
+                self.sim_path, self.snap, self.galaxy_id, "stars"
+            )
+            self._get_relative_coordinates(self._stars)
+            self._get_relative_distances(self._stars)
+            self._get_velocities(self._stars)
+        return self._stars
+
+    @property
+    def gas(self):
+        if self._gas is None:
+            self._gas = il.snapshot.loadHalo(
+                self.sim_path, self.snap, self.halo_id, "gas"
+            )
+            if self.with_vesc:
+                self._get_gas_v_esc(self)
+
+            self._get_relative_coordinates(self._gas)
+            self._get_relative_distances(self._gas)
+            self._gas = self._cut_gal_scale(self._gas)
+            self._get_velocities(self._gas)
+            self._get_dir(self._gas)
+            self._get_flow(self._gas)
+            self._get_rot_vel(self._gas)
+            gas_temperature.gasTemp(self._gas)
+            self._get_hsml()
+        return self._gas
+
+    def _cut_gal_scale(self, gas):
+        max_r = self.cut_factor * self.scale_radius
+        if max_r > self.r_vir:
+            max_r = self.r_vir
+        relevant_gas = gas["Relative_Distances"] < max_r
+        gas = map_to_new_dict(gas, relevant_gas)
+        return gas
+
+    def _get_relative_coordinates(self, particles):
         particles["Relative_Coordinates"] = (
             particles["Coordinates"] - self.gal_pos
         )
         return
 
-    def get_relative_distances(self, particles):
+    def _get_relative_distances(self, particles):
         particles["Relative_Distances"] = np.sqrt(
             np.sum(np.square(particles["Relative_Coordinates"]), axis=1)
         )
         return
 
-    def retrieve_galaxy_stars(self):
-        self.stars = il.snapshot.loadSubhalo(
-            self.sim_path, self.snap, self.galaxy_id, "stars"
-        )
-        self.get_relative_coordinates(self.stars)
-        self.get_relative_distances(self.stars)
-        self.get_velocities(self.stars)
-        return
-
-    def get_velocities(self, particles):
+    def _get_velocities(self, particles):
         self.z = get_redshift(self.snap)
         particles["Velocities"] = particles["Velocities"] * np.sqrt(
             scale_factor(self.z)
         )
 
-        self.get_galaxy_vel()
         particles["Relative_Velocities"] = (
-            particles["Velocities"] - self.galaxy_vel.T
+            particles["Velocities"] - self.gal_vel.T
         )
         particles["Relative_Velocities_abs"] = np.linalg.norm(
             particles["Relative_Velocities"], axis=1
         )
         return
 
-    def get_rel_coord_dist(self):
-        self.get_relative_coordinates(self.gas)
-        self.gas["Direction"] = (
-            self.gas["Relative_Coordinates"].T
-            / np.linalg.norm(self.gas["Relative_Coordinates"], axis=1)
+    def _get_dir(self, gas):
+        gas["Direction"] = (
+            gas["Relative_Coordinates"].T
+            / np.linalg.norm(gas["Relative_Coordinates"], axis=1)
         ).T
+        return
 
-    def get_flow(self):
+    def _get_flow(self, gas):
         # >0 means outflow, <0 means infall
-        self.gas["Flow_Velocities"] = np.float32(
-            np.multiply(
-                self.gas["Relative_Velocities"], self.gas["Direction"]
-            ).sum(axis=1)
+        gas["Flow_Velocities"] = np.float32(
+            np.multiply(gas["Relative_Velocities"], gas["Direction"]).sum(
+                axis=1
+            )
         )
+        return
 
-    def get_rot_vel(self):
-        self.gas["Rot_Velocities"] = np.sqrt(
-            self.gas["Relative_Velocities_abs"] ** 2
-            - self.gas["Flow_Velocities"] ** 2
+    def _get_rot_vel(self, gas):
+        gas["Rot_Velocities"] = np.sqrt(
+            gas["Relative_Velocities_abs"] ** 2 - gas["Flow_Velocities"] ** 2
         )
-        self.gas["Rot_Velocities"][np.isnan(self.gas["Rot_Velocities"])] = 0
+        gas["Rot_Velocities"][np.isnan(gas["Rot_Velocities"])] = 0
+        return
 
-    def get_hsml(self):
+    def _get_hsml(self):
         self.gas["hsml"] = 2.5 * (
             3 * (self.gas["Masses"] / self.gas["Density"]) / (4.0 * np.pi)
         ) ** (1.0 / 3)
-
-    def retrieve_halo_gas(self):
-        self.gas = il.snapshot.loadHalo(
-            self.sim_path, self.snap, self.halo_id, "gas"
-        )
-        if self.with_vesc:
-            self.get_gas_v_esc(self)
-
-        self.get_rel_coord_dist()
-        self.get_velocities(self.gas)
-        self.get_flow()
-        self.get_rot_vel()
-        gas_temperature.gasTemp(self.gas)
-        self.get_relative_distances(self.gas)
-        self.get_hsml()
         return
 
     def group_gas(self):
@@ -275,10 +302,7 @@ class Galaxy:
         return rel_gas
 
     def get_angular_momentum_direction(self):
-        self.retrieve_galaxy_stars()
-        idces_rel_stars = (
-            self.stars["Relative_Distances"] < 5 * self.scale_radius
-        )
+        idces_rel_stars = self.stars["Relative_Distances"] < self.scale_radius
         rel_stars = map_to_new_dict(self.stars, idces_rel_stars)
         ang_mom = (
             np.cross(
@@ -294,7 +318,7 @@ class Galaxy:
     def rot_matrix_from_ang_mom(self):
         z_axis = np.array([[0, 0, 1]])
         self.ang_mom_dir = np.array([self.ang_mom_dir])
-        self.rotation, _ = R.align_vectors(self.ang_mom_dir, z_axis)
+        self.rotation, _ = R.align_vectors(z_axis, self.ang_mom_dir)
         return
 
     def rotate_into_galactic_plane(self):
@@ -306,14 +330,6 @@ class Galaxy:
         self.gas["Relative_Velocities"] = self.rotation.apply(
             self.gas["Relative_Velocities"]
         )
-        return
-
-    def cut_gal_scale(self):
-        max_r = self.cut_factor * self.scale_radius
-        if max_r > self.r_vir:
-            max_r = self.r_vir
-        relevant_gas = self.gas["Relative_Distances"] < max_r
-        self.gas = map_to_new_dict(self.gas, relevant_gas)
         return
 
     # This is very hacky and should be adjusted in the future
@@ -328,3 +344,4 @@ class Galaxy:
                 self.out_gas["Abs_Coordinates"] = np.copy(
                     self.out_gas["Coordinates"]
                 )
+        return
