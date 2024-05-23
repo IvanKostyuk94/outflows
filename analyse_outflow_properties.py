a2b9709cfc9df99c01568e6567295dbf9c0fcc66
Ivan Kostyuk <kostyuk@hotmail.de>
2024-05-23 17:20:29 +0200

    Fixed bug where self.df was used instead of self._df

--- analyse_outflow_properties.py
+++ analyse_outflow_properties.py
@@ -9,7 +9,7 @@ class OutflowPropUpdater:
     def __init__(
         self, df_name, snap=None, save_name=None, n_peak=3, group_props=None
     ):
-        self.df_name = df_name
+        self.df_name = df_name + config["hdf_ending"]
         self.snap = snap
         self.save_name = save_name
 
@@ -40,8 +40,8 @@ class OutflowPropUpdater:
     @property
     def df(self):
         if self._df is None:
-            self.df = pd.read_hdf(self.df_path)
-        return
+            self._df = pd.read_hdf(self.df_path)
+        return self._df
 
     def outflow_props(self, halo_id, snap):
         gal = Galaxy(
@@ -58,25 +58,46 @@ class OutflowPropUpdater:
             weighting="Luminosity"
         )
         out_props["v_mass"] = gal.get_average_outflow_vel(weighting="Masses")
+        out_props["M_out_cold"] = gal.get_outflow_mass(cold_only=True)
+        out_props["M_dot_cold"] = gal.get_flow_rate(cold_only=True)
+        out_props["v_lum_cold"] = gal.get_average_outflow_vel(
+            weighting="Luminosity", cold_only=True
+        )
+        out_props["v_mass_cold"] = gal.get_average_outflow_vel(
+            weighting="Masses", cold_only=True
+        )
         return out_props
+
+    def create_key_column(self, key):
+        if key not in self.df.keys():
+            self.df[key] = np.nan * np.ones(len(self.df))
+        return
 
     def add_outflow_parameters(self):
         if "v_out_mean" not in self.df.keys():
             self.df["v_out_mean"] = np.nan * np.ones(len(self.df))
         if "M_out" not in self.df.keys():
             self.df["M_out"] = np.nan * np.ones(len(self.df))
-
         if self.snap is not None:
             iteration_df = self.df[self.df.snap == self.snap]
         else:
             iteration_df = self.df
-        for _, element in iteration_df:
+        for _, element in iteration_df.iterrows():
             halo_id = int(element.Halo_id)
-            snap = element.snap
+            snap = int(element.snap)
             out_props = self.outflow_props(halo_id, snap)
-            sel_gal = self.df.loc[
-                (self.df.snap == snap) & (self.df.Halo_id == halo_id)
-            ]
-            for key in sel_gal.keys():
-                sel_gal[key] = out_props[key]
+            for key in out_props.keys():
+                self.create_key_column(key)
+                self.df.loc[
+                    (self.df.snap == snap) & (self.df.Halo_id == halo_id), key
+                ] = out_props[key]
         return
+
+    def save_df(self):
+        self.df.to_hdf(self.save_path, "galaxies")
+
+
+if __name__ == "__main__":
+    updater = OutflowPropUpdater("all_galaxies_new")
+    updater.add_outflow_parameters()
+    updater.save_df()
