b775f042b25fa3ee2f2f52b2077d245b889be883
Ivan Kostyuk <kostyuk@hotmail.de>
2024-09-02 12:26:18 +0200

    Added new functionalities for outflow width analysis

--- analyse_outflow_properties.py
+++ analyse_outflow_properties.py
@@ -4,6 +4,7 @@ import pandas as pd
 from config import config
 from process_gas import Galaxy
 from galaxy_shell_outflows import GalaxyShells
+from los_projection import GalaxyProjections
 
 
 class OutflowPropUpdater:
@@ -51,6 +52,14 @@ class OutflowPropUpdater:
     def df(self):
         if self._df is None:
             self._df = pd.read_hdf(self.df_path)
+            # quick hack to fix a problem, this should be uncommented
+
+            # self._df = self._df[
+            #     (self.df.snap >= self.snap_range[0])
+            #     & (self.df.snap <= self.snap_range[1])
+            #     & (self.df.M_star_log > 7.5)
+            # ].copy(deep=True)
+
         return self._df
 
     def outflow_props(self, halo_id, snap):
@@ -74,12 +83,8 @@ class OutflowPropUpdater:
         try:
             out_props["M_out"] = gal.get_outflow_mass()
             out_props["M_dot"] = gal.get_flow_rate()
-            out_props["v_lum"] = gal.get_average_outflow_vel(
-                weighting="Luminosity"
-            )
-            out_props["v_mass"] = gal.get_average_outflow_vel(
-                weighting="Masses"
-            )
+            out_props["v_lum"] = gal.get_average_outflow_vel(weighting="Luminosity")
+            out_props["v_mass"] = gal.get_average_outflow_vel(weighting="Masses")
             out_props["M_out_cold"] = gal.get_outflow_mass(cold_only=True)
             out_props["M_dot_cold"] = gal.get_flow_rate(cold_only=True)
             out_props["v_lum_cold"] = gal.get_average_outflow_vel(
@@ -158,11 +163,7 @@ class OutflowPropUpdater:
 
     def add_outflow_parameters(self):
         if self.snap_range is not None:
-            iteration_df = self.df[
-                (self.df.snap >= self.snap_range[0])
-                & (self.df.snap <= self.snap_range[1])
-                & (self.df.M_star_log > 7.5)
-            ]
+            iteration_df = self.df
         else:
             iteration_df = self.df
         counter = 0
@@ -183,25 +184,21 @@ class OutflowPropUpdater:
                 ] = out_props[key]
 
             if counter % 100 == 0:
-                print(
-                    f"Processed {counter/len(iteration_df)*100:.2f}% of galaxies"
-                )
+                print(f"Processed {counter/len(iteration_df)*100:.2f}% of galaxies")
                 self.save_df()
         return
 
     def add_outflow_metallicity(self):
-        if self.snap_range is not None:
-            iteration_df = self.df[
-                (self.df.snap >= self.snap_range[0])
-                & (self.df.snap <= self.snap_range[1])
-                & (self.df.M_star_log > 7.5)
-            ]
-        else:
-            iteration_df = self.df
+        # if self.snap_range is not None:
+        #     iteration_df = self.df
+        # else:
+        iteration_df = self.df
         counter = 0
         if "outflow_Z" not in self.df.keys():
             self.df["outflow_Z"] = np.nan * np.ones(len(self.df))
-            self.df["outflow_Z_warm"] = np.nan * np.ones(len(self.df))
+            self.df["remain_Z"] = np.nan * np.ones(len(self.df))
+
+            # self.df["outflow_Z_warm"] = np.nan * np.ones(len(self.df))
 
         for _, element in iteration_df.iterrows():
             counter += 1
@@ -213,20 +210,107 @@ class OutflowPropUpdater:
                 snap=snap,
                 group_props=self.group_props,
             )
-            self.df.loc[
-                (self.df.snap == snap) & (self.df.Halo_id == halo_id),
-                "outflow_Z",
-            ] = gal.get_outflow_metallicity(cold_only=False)
+            try:
+                self.df.loc[
+                    (self.df.snap == snap) & (self.df.Halo_id == halo_id),
+                    "outflow_Z",
+                ] = gal.get_outflow_metallicity(cold_only=False, type="out")
 
-            self.df.loc[
-                (self.df.snap == snap) & (self.df.Halo_id == halo_id),
-                "outflow_Z_warm",
-            ] = gal.get_outflow_metallicity(cold_only=True)
+                self.df.loc[
+                    (self.df.snap == snap) & (self.df.Halo_id == halo_id),
+                    "remain_Z",
+                ] = gal.get_outflow_metallicity(cold_only=False, type="remain")
+            except:
+                self.df.loc[
+                    (self.df.snap == snap) & (self.df.Halo_id == halo_id),
+                    "outflow_Z",
+                ] = np.nan
+
+                self.df.loc[
+                    (self.df.snap == snap) & (self.df.Halo_id == halo_id),
+                    "remain_Z",
+                ] = np.nan
 
             if counter % 100 == 0:
-                print(
-                    f"Processed {counter/len(iteration_df)*100:.2f}% of galaxies"
-                )
+                print(f"Processed {counter/len(iteration_df)*100:.2f}% of galaxies")
+                self.save_df()
+        return
+
+    def get_offset_W80(self, gas):
+        try:
+            v_range = np.quantile(gas["Relative_Velocities"], [0.05, 0.1, 0.9, 0.95])
+            delta_v = (v_range[3] + v_range[0]) / 2
+            W80 = v_range[2] - v_range[1]
+        except:
+            delta_v = np.nan
+            W80 = np.nan
+        return delta_v, W80
+
+    def add_outflow_W80(self):
+        # quick hack should be removed afterwards
+        iteration_df = self.df[
+            (self.df.snap >= self.snap_range[0])
+            & (self.df.snap <= self.snap_range[1])
+            & (self.df.M_star_log > 7.5)
+        ]
+        # iteration_df = self.df
+        counter = 0
+        phi_angles = [0, 30, 60, 90]
+        theta_angles = [0, 30, 60, 90, 120, 150]
+        keys = ["W80_outflow", "delta_v_outflow", "W80_galaxy", "delta_v_galaxy"]
+        for phi in phi_angles:
+            for theta in theta_angles:
+                for key in keys:
+                    if f"{key}_{phi}_{theta}" not in self.df.keys():
+                        self.df[f"{key}_{phi}_{theta}"] = np.nan * np.ones(len(self.df))
+
+        for _, element in iteration_df.iterrows():
+            counter += 1
+            halo_id = int(element.Halo_id)
+            snap = int(element.snap)
+            for phi in phi_angles:
+                for theta in theta_angles:
+                    gal = GalaxyProjections(
+                        df=self.df,
+                        halo_id=halo_id,
+                        snap=snap,
+                        projection_angle_theta=theta,
+                        projection_angle_phi=phi,
+                    )
+                    try:
+                        gal.line_of_sight_projection()
+                        gal.project_outflows()
+
+                        delta_v_galaxy, W80_galaxy = self.get_offset_W80(gal.remain_gas)
+                        delta_v_outflow, W80_outflow = self.get_offset_W80(gal.out_gas)
+                    except:
+                        delta_v_galaxy = np.nan
+                        W80_galaxy = np.nan
+                        delta_v_outflow = np.nan
+                        W80_outflow = np.nan
+
+                    self.df.loc[
+                        (self.df.snap == snap) & (self.df.Halo_id == halo_id),
+                        f"delta_v_galaxy_{phi}_{theta}",
+                    ] = delta_v_galaxy
+
+                    self.df.loc[
+                        (self.df.snap == snap) & (self.df.Halo_id == halo_id),
+                        f"W80_galaxy_{phi}_{theta}",
+                    ] = W80_galaxy
+
+                    self.df.loc[
+                        (self.df.snap == snap) & (self.df.Halo_id == halo_id),
+                        f"delta_v_outflow_{phi}_{theta}",
+                    ] = delta_v_outflow
+
+                    self.df.loc[
+                        (self.df.snap == snap) & (self.df.Halo_id == halo_id),
+                        f"W80_outflow_{phi}_{theta}",
+                    ] = W80_outflow
+
+            if counter % 100 == 0:
+                print(f"Processed {counter/len(iteration_df)*100:.2f}% of galaxies")
                 self.save_df()
         return
 
@@ -236,13 +320,18 @@ class OutflowPropUpdater:
 
 if __name__ == "__main__":
     updater = OutflowPropUpdater(
-        "all_galaxies_extended",
-        save_name="all_galaxies_extended",
-        snap_range=[17, 25],
+        # "all_galaxies_extended",
+        "updated_with_W80_angles",
+        # "updated_with_M_out",
+        # save_name="test",
+        save_name="updated_with_W80_angles_full",
+        snap_range=[23, 25],
+        # snap_range=[0, 5],
         with_quantile=False,
         only_shell=False,
     )
-    updater.add_outflow_metallicity()
-
     # updater.add_outflow_parameters()
+    updater.add_outflow_W80()
+    # updater.add_outflow_metallicity()
+
     updater.save_df()
