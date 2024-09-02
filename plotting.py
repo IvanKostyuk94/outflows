200f9276b2304b25bcf735e5970e4ec63c577d9e
Ivan Kostyuk <kostyuk@hotmail.de>
2024-09-02 12:26:58 +0200

    Added new plotting functions in particular 2D histograms

--- plotting.py
+++ plotting.py
@@ -10,6 +10,9 @@ from Grid_halo import GasGridder
 from process_gas import Galaxy
 from los_projection import GalaxyProjections
 from utils import get_halo, get_redshift
+import scipy
+from scipy import stats
+from functools import partial
 
 
 def prop_labels(prop):
@@ -60,6 +63,14 @@ def prop_labels(prop):
         "Luminosity_light": r"$L_{H\alpha, \mathrm{dist}}$[a.u.]",
         "Luminosity_O3": r"$L_{OIII}$[a.u.]",
         "v_z": r"$v_z[\mathrm{km}/\mathrm{s}]$",
+        "Distance": r"$M$[a.u.]",
+        "M_out_log":r"$M_\mathrm{out}[\log(M_\odot)]$",
+        "M_gas_log":r"$M_\mathrm{gas}[\log(M_\odot)]$",
+        "SFR_log":r"$\mathrm{SFR}[\log(M_\odot/\mathrm{yr})]$",
+        "W80_galaxy":r"$W_{80, \mathrm{gal}}[\log( \mathrm{km}/\mathrm{s} )]$",
+        "W80_outflow":r"$W_{80, \mathrm{out}}[\log( \mathrm{km}/\mathrm{s} )]$",
+        "W_ratio":r"$W_{80, \mathrm{out}}/W_{80, \mathrm{gal}}$",
+        "Z_ratio":r"$Z_\mathrm{out}/Z_\mathrm{gal}$",
     }
     return prop_labels[prop]
 
@@ -99,15 +110,15 @@ def get_ranges(prop, parameters):
         parameters["vcenter"] = 300
         parameters["vmax"] = 600
 
-    elif prop == "Masses":
-        parameters["vmin"] = 7.0
-        parameters["vcenter"] = 8.0
-        parameters["vmax"] = 9.0
-
     # elif prop == "Masses":
-    #     parameters["vmin"] = 6.0
-    #     parameters["vcenter"] = 7.5
-    #     parameters["vmax"] = 9
+    #     parameters["vmin"] = 7.0
+    #     parameters["vcenter"] = 8.0
+    #     parameters["vmax"] = 9.0
+
+    elif prop == "Masses":
+        parameters["vmin"] = 6.0
+        parameters["vcenter"] = 7.5
+        parameters["vmax"] = 9
 
     elif prop == "GFM_Metallicity":
         parameters["vmin"] = -3.5
@@ -388,11 +399,15 @@ def plot_prop_correlation(
 
 def get_weights(gas, weighting):
     if weighting == "Luminosity":
-        weights = gas["Density"] * gas["Masses"]
+        weights = (gas["Density"] * gas["Masses"]) / gas["Density"].mean()
     elif weighting == "Luminosity_light":
         weights = gas["Density"] * gas["Masses"] / gas["SFR_dist"] ** 2
+        test = gas["Density"] / gas["SFR_dist"] ** 2
+        weights = gas["Density"] * gas["Masses"] / gas["SFR_dist"] ** 2 / test.mean()
     elif weighting == "Luminosity_O3":
         weights = gas["Density"] * gas["Masses"] * gas["GFM_Metallicity"]
+    elif weighting == "Distance":
+        weights = gas["Masses"] / gas["SFR_dist"] ** 2
     elif weighting is None:
         weights = np.ones_like(gas["Flow_Velocities"])
     else:
@@ -413,11 +428,25 @@ def get_histogram(gas, velocity_type, bins, weights):
     return heights
 
 
+def Gauss(x, a, x0, sigma):
+    return a * np.exp(-((x - x0) ** 2) / (2 * sigma**2))
+
+
+def Gauss2(x, a, x0_1, sigma, delta_a, x0_2, delta_sigma):
+    return (a * np.exp(-((x - x0_1) ** 2) / (2 * sigma**2))) + (
+        a * delta_a * np.exp(-((x - x0_2) ** 2) / (2 * (sigma * delta_sigma)**2))
+    )
+
+def get_reduced_chi_squared(x, y, y_err, model, popt):
+    chi_squared = np.sum((y - model(x, *popt)) ** 2 / y_err ** 2)
+    dof = len(x) - len(popt)
+    return chi_squared / dof, 
+
 def plot_velocity_histogram(
     gases,
     velocity_types,
     weighting="Luminosity",
-    bin_n=20,
+    bin_n=80,
     range=None,
     labels=None,
     norms=None,
@@ -429,35 +458,59 @@ def plot_velocity_histogram(
         norms = np.ones(len(gases))
     fig, ax = plt.subplots(figsize=(15, 10))
     if range is None:
-        bins = np.linspace(
-            gases[0][velocity_types[0]].min(),
-            gases[0][velocity_types[0]].max(),
-            bin_n,
-        )
+        # bins = np.linspace(
+        #     gases[0][velocity_types[0]].min(),
+        #     gases[0][velocity_types[0]].max(),
+        #     bin_n,
+        # )
+        bins = np.linspace(-1200, 1200, 80)    
+
     else:
         bins = np.linspace(range[0], range[1], bin_n + 1)
     centers = (bins[1:] + bins[:-1]) / 2
     widths = bins[1:] - bins[:-1]
+    tot_heights = np.zeros_like(centers)
     for i, gas in enumerate(gases):
-        weights = get_weights(gas, weighting)
-        heights = get_histogram(gas, velocity_types[i], bins, weights)
-
-        ax.bar(
-            centers,
-            heights,
-            width=widths,
-            label=labels[i],
-            alpha=0.3,
-        )
+        if i > 0:
+            weights = get_weights(gas, weighting=weighting[i])
+            heights = get_histogram(gas, velocity_types[i], bins, weights)
+            if i == 1:
+                ax.bar(
+                    centers,
+                    heights,
+                    width=widths,
+                    label=labels[i],
+                    alpha=0.3,
+                )
+            tot_heights = tot_heights + heights
+    ax.bar(centers, tot_heights, width=widths, label="summed", alpha=0.3)
+    # try:
+    bounds_1 = ([0, -np.inf, 0], [np.inf, np.inf, 150])
+    popt, pcov = scipy.optimize.curve_fit(
+        Gauss, centers, heights, p0=[max(heights), 0, 10], bounds=bounds_1
+    )
+    bounds_2 = (
+        [0, -np.inf, 0, 0, -np.inf, 1.2],
+        [np.inf, np.inf, 150, 0.5, np.inf, np.inf],
+    )
+    popt2, pcov2 = scipy.optimize.curve_fit(
+        Gauss2, centers, heights, p0=[max(heights), 0, 30, 0.3, 0, 2], bounds=bounds_2
+    )
+    pop_gauss1 = popt2[:3]
+    pop_gauss2 = [popt2[0]*popt2[3], popt2[4], popt2[5]*popt2[2]]
+    ax.plot(centers, Gauss(centers, *popt), "r-", label="fit Gauss")
+    ax.plot(centers, Gauss(centers, *pop_gauss1), "b-", label="fit 2 Gauss 1")
+    ax.plot(centers, Gauss(centers, *pop_gauss2), "g-", label="fit 2 Gauss 2")
+    # except:
+        # pass
 
     parameters = plot_parameters_comp()
     ax.set_xlabel(
         prop_labels(velocity_types[-1]), fontsize=parameters["label_fontsize"]
     )
-    ax.set_ylabel(
-        prop_labels(weighting), fontsize=parameters["label_fontsize"]
-    )
+    ax.set_ylabel(prop_labels(weighting[i]), fontsize=parameters["label_fontsize"])
     ax.tick_params(labelsize=parameters["ticklabelsize"])
+
     ax.legend(fontsize=15)
     # ax.set_yscale("log")
     if title is not None:
@@ -491,11 +544,9 @@ def plot_density_histogram(
     )
 
     parameters = plot_parameters_comp()
-    ax.set_xlabel(
-        r"$n_e[\mathrm{cm}^{-3}]$", fontsize=parameters["label_fontsize"]
-    )
+    ax.set_xlabel(r"$n_e[\mathrm{cm}^{-3}]$", fontsize=parameters["label_fontsize"])
     ax.set_ylabel(
-        "counts",
+        r"$\log(n_\mathrm{galaxies}$",
         fontsize=parameters["label_fontsize"],
     )
     ax.tick_params(labelsize=parameters["ticklabelsize"])
@@ -506,9 +557,7 @@ def plot_density_histogram(
     return
 
 
-def plot_los_histograms(
-    halo_id, snap, df, angles_theta, angles_phi, bin_n=100
-):
+def plot_los_histograms(halo_id, snap, df, angles_theta, angles_phi, bin_n=100):
     columns = len(angles_theta)
     rows = len(angles_phi)
     figsize = (5 * columns, 5 * rows)
@@ -565,6 +614,181 @@ def plot_los_histograms(
                 alpha=0.3,
             )
 
+def create_color_bar(
+    f,
+    ax,
+    subfig,
+    label=None,
+    multiple=False,
+    horizontal=False,
+    gap=False,
+    prop="f_esc",
+):
+
+    cbar = f.colorbar(subfig, cax=ax)
+    ax.xaxis.set_ticks_position("top")
+    ax.xaxis.set_label_position("top")
+
+    size = 25
+    cbar.set_label(label, size=size, labelpad=18)
+    ticksize = 15
+    cbar.ax.tick_params(labelsize=ticksize)
+    if prop != "f_esc":
+        cbar.ax.set_xticks([0.5, 0.75, 1, 1.5, 2], labelsize=ticksize)
+    return
+
+def get_quantile(array, quantile):
+    return np.percentile(array, quantile)
+
+def get_histogram(
+    df,
+    x_values,
+    y_values,
+    bins,
+    color_prop="M_out",
+    statistic="counts",
+    quant = None, 
+):
+    if statistic == "quantile":
+        statistic = partial(get_quantile, quantile = quant)
+    hist, *_ = stats.binned_statistic_2d(
+        x_values,
+        y_values,
+        values=df[color_prop],
+        statistic=statistic,
+        bins=bins,
+    )
+
+    hist_cont, xedges_cont, yedges_cont, _ = stats.binned_statistic_2d(
+        x_values,
+        y_values,
+        values=df[color_prop],
+        statistic="count",
+        bins=bins,
+    )
+
+    if statistic == "count":
+        pass
+        # hist = np.log10(hist)
+    return hist, hist_cont, xedges_cont, yedges_cont
+
+def prop_prop_histogram(
+    df,
+    prop_x,
+    prop_y,
+    color_prop="M_out",
+    statistic="counts",
+    em_weighted=False,
+    log_x=False,
+    log_y=False,
+    bins_x=12,
+    bins_y=12,
+    color_log=False,
+    contour=True,
+    quantile=None, 
+):
+    if color_log:
+        df["color_prop"] = np.log10(df[color_prop])
+        color_prop = "color_prop"
+
+    parameters = plot_parameters_comp()
+
+    df.dropna(subset=prop_x, inplace=True)
+    df.dropna(subset=prop_y, inplace=True)
+
+    x_values = df[prop_x]
+    y_values = df[prop_y]
+    if log_x:
+        x_values = np.ma.masked_invalid(np.log10(x_values))
+    if log_y:
+        y_values = np.ma.masked_invalid(np.log10(y_values))
+
+    x_edges = np.linspace(x_values.min(), x_values.max(), bins_x)
+    y_edges = np.linspace(y_values.min(), y_values.max(), bins_y)
+
+    hist, hist_cont, xedges_cont, yedges_cont = get_histogram(
+        df,
+        x_values,
+        y_values,
+        bins=[x_edges, y_edges],
+        color_prop=color_prop,
+        statistic=statistic,
+        quant=quantile
+    )
+    old_hist = np.copy(hist)
+    if statistic == "count":
+        hist = np.log10(hist)
+
+    cont_centers_x = (xedges_cont[1:] + xedges_cont[:-1]) / 2
+    cont_centers_y = (yedges_cont[1:] + yedges_cont[:-1]) / 2
+    x_grid, y_grid = np.meshgrid(x_edges, y_edges)
+    # vmin, vcenter, vmax = get_color_limits(color_prop, statistic)
+    # col_norm = colors.TwoSlopeNorm(vmin=vmin, vcenter=vcenter, vmax=vmax)
+    if statistic == "count":
+        col_norm = colors.TwoSlopeNorm(vmin=0, vcenter=1.5, vmax=3)
+    else:
+        col_norm = colors.TwoSlopeNorm(vmin=6, vcenter=8, vmax=10)
+    f, axs = plt.subplots(
+        ncols=2,
+        nrows=1,
+        gridspec_kw={
+            "hspace": 0.1,
+            "wspace": 0.1,  # 0.3 * 0.75 * len(props_of_interest),
+            "width_ratios": [24, 1],
+            "height_ratios": [24],
+        },
+        figsize=[10, 8],
+    )
+    ax = axs[0]
+    cax = axs[1]
+    subfig = ax.pcolormesh(
+        x_grid, y_grid, hist.T, norm=col_norm, cmap=plt.get_cmap("inferno")
+    )
+    for i in range(len(xedges_cont) - 1):
+        for j in range(len(yedges_cont) - 1):
+            label_base = old_hist[i, j]
+            if statistic == "count":
+                count = int(label_base)
+            else:
+                count = f"{label_base:.2f}"
+            if label_base > 0:  # Only annotate non-zero bins
+                ax.text(xedges_cont[i] + (xedges_cont[i + 1] - xedges_cont[i]) / 2,
+                        yedges_cont[j] + (yedges_cont[j + 1] - yedges_cont[j]) / 2,
+                        count,
+                        color='blue',
+                        ha='center', va='center')
+    # levels = get_levels(hist_cont, thresholds=[0.954, 0.683])
+    # if contour:
+    #     ax.contour(
+    #         cont_centers_x,
+    #         cont_centers_y,
+    #         hist_cont.T,
+    #         levels=levels,
+    #         linewidths=4,
+    #         linestyles=["dotted", "dashed", "solid"],
+    #         colors="lightblue",
+    #     )
+    if statistic == "count":
+        color_label = r"$\log(n_\mathrm{galaxies})$"
+    else:
+        if statistic == "quantile":
+            color_label = fr"$M_{{\mathrm{{out,}} {quantile}}}[\log(M_\odot)]$"
+            # color_label = prop_labels(color_prop) + f" {quantile} quantile"
+        else:
+            color_label = prop_labels(color_prop)
+    create_color_bar(
+        f,
+        cax,
+        subfig=subfig,
+        label=color_label,
+        gap=True,
+        horizontal=False,
+    )
+    ax.tick_params(labelsize=15)
+
+    ax.set_xlabel(prop_labels(prop_x), size=25)
+    ax.set_ylabel(prop_labels(prop_y), size=25)
+    return
 
 def plot_prop_maps_grouped(
     halo_id,
@@ -595,4 +819,120 @@ def plot_prop_maps_grouped(
     )
     for prop in props:
         plot_prop_maps(gridder, prop, dirs, sizebar_length)
+    return
+
+
+
+def get_jades_data():
+    data = {}
+    data['M_star_log_Oiii'] = np.array([7.69 ,7.60, 7.85, 8.09, 7.78, 8.63])
+    data['M_star_log_Ha'] = np.array([8.54, 8.11, 7.73, 8.28, 7.81, 7.93, 7.85, 8.24]) 
+    data['SFR_log_Oiii'] = np.array([0.09, 0.53, 0.61, 0.39, 0.41, 1.14])
+    data['SFR_log_Ha'] = np.array([0.65, 0.74, 0.14, 0.34, 0.09, -0.67, 0.61, 0.01]) 
+    data['M_out_log_Oiii'] = np.array([6.46 , 7.07, 6.84, 6.56, 7.12, 8.26])
+    data['M_out_log_Ha'] = np.array([6.74, 7.17, 6.00, 6.54, 6.03, 5.85, 6.67, 6.51])
+    return data
+
+def prop_prop_scatter(
+    df,
+    prop_x,
+    prop_y,
+    color_prop="M_out",
+    log_x=False,
+    log_y=False,
+):
+    parameters = plot_parameters_comp()
+
+    df.dropna(subset=prop_x, inplace=True)
+    df.dropna(subset=prop_y, inplace=True)
+
+    x_values = df[prop_x]
+    y_values = df[prop_y]
+    if log_x:
+        x_values = np.ma.masked_invalid(np.log10(x_values))
+    if log_y:
+        y_values = np.ma.masked_invalid(np.log10(y_values))
+
+    col_norm = colors.TwoSlopeNorm(vmin=6, vcenter=8, vmax=10)
+    data = get_jades_data()
+    
+    # f, axs = plt.subplots(
+    #     ncols=2,
+    #     nrows=1,
+    #     gridspec_kw={
+    #         "hspace": 0.1,
+    #         "wspace": 0.1,  # 0.3 * 0.75 * len(props_of_interest),
+    #         "width_ratios": [24, 1],
+    #         "height_ratios": [24],
+    #     },
+    #     figsize=[10, 8],
+    # )
+    f, ax = plt.subplots(
+        ncols=1,
+        nrows=1,
+        gridspec_kw={
+            "hspace": 0.1,
+            "wspace": 0.1,  # 0.3 * 0.75 * len(props_of_interest),
+            "width_ratios": [24],
+            "height_ratios": [24],
+        },
+        figsize=[10, 8],
+    )
+    # ax = axs[0]
+    # cax = axs[1]
+    subfig = ax.scatter(
+        x_values, y_values, s=1, color = 'b', alpha=0.3, label='TNG data'
+    )
+    ax.scatter(data['M_star_log_Oiii'], data['M_out_log_Oiii'], s=100, color = 'r', marker='*', label='JADES data OIII')
+    ax.scatter(data['M_star_log_Ha'], data['M_out_log_Ha'], s=100, color = 'yellow', marker='P', label=r'JADES data H$\alpha$')
+    
+    color_label = prop_labels(color_prop)
+    # create_color_bar(
+    #     f,
+    #     cax,
+    #     subfig=subfig,
+    #     label=color_label,
+    #     gap=True,
+    #     horizontal=False,
+    # )
+    ax.tick_params(labelsize=15)
+    ax.legend(fontsize=15)
+
+    ax.set_xlabel(prop_labels(prop_x), size=25)
+    ax.set_ylabel(prop_labels(prop_y), size=25)
+    return
+
+def get_detection_fraction(df, thresholds, bins=20):
+    df["bin"] = pd.cut(df['M_star_log'], bins=bins)
+    fractions = []
+    for threshold in thresholds:
+        is_larger = df.groupby('bin')['M_out_log'].apply(np.array).apply(lambda x: x > threshold)
+        fraction = []
+        centers = []
+        for bin in df['bin'].unique():
+            centers.append((bin.left+bin.right)/2)
+        centers = np.sort(centers)
+
+        for array in is_larger:
+            fraction.append(np.sum(array)/len(array))
+        fractions.append(fraction)
+
+    f, ax = plt.subplots(
+        ncols=1,
+        nrows=1,
+        gridspec_kw={
+            "hspace": 0.1,
+            "wspace": 0.1,  # 0.3 * 0.75 * len(props_of_interest),
+            "width_ratios": [24],
+            "height_ratios": [24],
+        },
+        figsize=[10, 8],
+    )
+    for i, element in enumerate(fractions):
+        ax.plot(centers, element, label = rf'$10^{thresholds[i]}M_\odot$ threshold')
+    ax.tick_params(labelsize=15)
+    ax.legend(fontsize=15)
+
+    ax.set_xlabel(prop_labels('M_out_log'), size=25)
+    ax.set_ylabel('Fraction of detectable outflows', size=25)
     return
